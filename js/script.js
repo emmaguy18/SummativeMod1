fd8c1385f290422acf39ce162b5abcd1e076c27a
Emma Guy <dusk@Vampiredusk.local>
2019-07-09 15:41:26 +1200

    edited dropdown nav

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 console.log("js has loaded");
-function showNav(){
-    var navHam = document.getElementById("navList");
-    if(navList.style.display === 'block'){
-        navList.style.display = 'none';
-    } else {
-        navList.style.display = 'block';
-    }
-}
+// function showNav(){
+//     var navHam = document.getElementById("navList");
+//     if(navList.style.display === 'block'){
+//         navList.style.display = 'none';
+//     } else {
+//         navList.style.display = 'block';
+//     }
+// }
